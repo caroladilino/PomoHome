11130f64b8bd832e6565062e883e6ceb15f1f93f
Enzo Kretzer <kretzer.enzo60@gmail.com>
2026-06-17 12:04:20 -0300

    fix: change house name

--- core/src/main/java/io/github/PomoHome/screens/TelaJogo.java
+++ core/src/main/java/io/github/PomoHome/screens/TelaJogo.java
@@ -21,6 +21,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import io.github.PomoHome.Main;
@@ -243,6 +244,14 @@ public class TelaJogo implements Screen {
     private void criarChromeEdicao() {
         campoNome = new TextField("", main.getSkin());
         campoNome.setAlignment(com.badlogic.gdx.utils.Align.center);
+        campoNome.addListener(new FocusListener() {
+            @Override
+            public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
+                if (!focused && viewAtual == ModoPainel.INVENTARIO) {
+                    commitNomeCasa(casaDoJogador());
+                }
+            }
+        });
 
         btnRemover = botaoRosa("X");
         aoClicar(btnRemover, () -> {
